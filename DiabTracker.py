662e0d0738448f3116e3a338097700a1435620e9
Enigma7484 <omarhaque7484@gmail.com>
2023-01-21 00:29:51 -0500

    Continuing on IntelliJ
    Commit 2:
    
    Just read the .csv

--- DiabTracker.py
+++ DiabTracker.py
@@ -5,3 +5,9 @@ from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 
+# loading the diabetes dataset to a pandas DataFrame
+diabetes_dataset = pd.read_csv('diabetes.csv')
+
+# pd.read_csv
+# printing the first 5 rows of the dataset
+print(diabetes_dataset.head())
